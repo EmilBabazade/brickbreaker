6fb53b1554ddd3f623e2f2c30364b6871ca82512
Emil <emilbabazade29@gmail.com>
2025-01-20 16:56:58 +0200

    ball movement

---
+++ scenes/ball/ball.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+@export var speed = 600
+var direction: Vector2
+
+func _ready() -> void:
+	var rng = RandomNumberGenerator.new()
+	var random_num = rng.randi_range(0, 100)
+	direction = Vector2(0.3, -1) if random_num % 2 == 0 else Vector2(-0.3, 1)
+	velocity = direction * speed
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	var collision = move_and_collide(direction * speed * delta)
+	if collision:
+		var collider = collision.get_collider()
+		var collision_normal = Vector2.ZERO
+		if collider.is_in_group('entity'):
+			collision_normal = (global_position - collider.global_position).normalized()
+		else:
+			collision_normal = collision.get_normal()
+		#$AudioStreamPlayer.pitch_scale = randf_range(0.8, 1.2)
+		#$AudioStreamPlayer.play()
+		direction = direction.bounce(collision_normal).normalized()
