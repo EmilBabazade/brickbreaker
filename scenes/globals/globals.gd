5b5cd133fc3bed8b9696a2656d3b6529973e2390
Emil <emilbabazade29@gmail.com>
2025-01-20 19:30:46 +0200

    health ui

--- scenes/globals/globals.gd
+++ scenes/globals/globals.gd
@@ -1,6 +1,7 @@
 extends Node
 
 signal score_changed
+signal health_changed
 
 var score = 0:
 	get:
@@ -8,3 +9,10 @@ var score = 0:
 	set(value):
 		score = value
 		score_changed.emit()
+
+var health = 3:
+	get:
+		return health
+	set(value):
+		health = value
+		health_changed.emit()
--- scenes/user_interface/ui.gd
+++ scenes/user_interface/ui.gd
@@ -1,7 +1,13 @@
 extends CanvasLayer
 
 func _ready() -> void:
+	on_score_changed()
+	on_health_changed()
 	Globals.connect('score_changed', on_score_changed)
+	Globals.connect('health_changed', on_health_changed)
 
 func on_score_changed():
 	$Score.text = str(Globals.score)
+
+func on_health_changed():
+	$HBoxContainer/Health.text = str(Globals.health)
