2cf646430a6dc31479c2cdac81e68299dbe3e086
Emil <emilbabazade29@gmail.com>
2025-01-20 15:57:21 +0200

    player movement

---
+++ scenes/player/player.gd
@@ -0,0 +1,16 @@
+extends StaticBody2D
+
+@export var speed = 600
+var direction: Vector2 = Vector2.ZERO
+
+func _process(_delta: float) -> void:
+	get_input()
+
+func _physics_process(delta: float) -> void:
+	move(delta)
+
+func get_input():
+	direction.x = Input.get_axis('left', 'right')
+
+func move(delta: float):
+	move_and_collide(direction * delta * speed)
